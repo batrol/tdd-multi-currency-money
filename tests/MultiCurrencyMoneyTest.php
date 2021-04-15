0bafd7d7a1a31ed109eb1e2312234dc300db7273
Plinio Almeida <plinioalmeida20@gmail.com>
2021-04-15 22:58:34 +0200

    Privacy
    Refactoring multiplication test

--- tests/MultiCurrencyMoneyTest.php
+++ tests/MultiCurrencyMoneyTest.php
@@ -9,7 +9,7 @@ class MultiCurrencyMoneyTest extends TestCase
      * - TODO: add amounts in two different currencies and convert the result given a set of exchange rates
      * - Multiply price per shares and number of shares and return an amount
      * - Fix multiplication issue. Amount in dollars increases on each multiplication.
-     * - TODO: Fix encapsulation issue. Dollar has public properties
+     * - Fix encapsulation issue. Dollar has public properties
      * - Compare two Dollar objects (equals())
      * - TODO: hashCode() (applicable?)
      * - TODO: compare with null
@@ -19,13 +19,9 @@ class MultiCurrencyMoneyTest extends TestCase
     public function testMultiplication()
     {
         $dollar = new Dollar(5);
-        $product = $dollar->times(2);
 
-        $this->assertEquals(10, $product->amount);
-
-        $product = $dollar->times(3);
-
-        $this->assertEquals(15, $product->amount);
+        $this->assertEquals(new Dollar(10), $dollar->times(2));
+        $this->assertEquals(new Dollar(15), $dollar->times(3));
     }
 
     public function testEquality()
